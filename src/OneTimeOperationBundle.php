08ce117b85bd58322a625817b89ebe48e761a60f
Uladzislau Beliakou <beliakou.uladzislau@gmail.com>
2024-11-14 17:49:34 +0300

    create an empty symfony bundle

---
+++ src/OneTimeOperationBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ubeliakou\OneTimeOperationBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class OneTimeOperationBundle extends AbstractBundle
+{
+    /**
+     * @param string[] $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
+}
